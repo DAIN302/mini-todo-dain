aba20c3a6722828628fbb3f98510e7aebdf82d9f
DAIN302 <dbgnfk12@gmail.com>
2024-08-14 17:44:09 +0900

    study : mini todo

--- server/controller/Ctodo.js
+++ server/controller/Ctodo.js
@@ -1,3 +1,4 @@
+const { raw } = require('express');
 const { Todo } = require('../models');
 const { Op } = require('sequelize');
 
@@ -27,44 +28,97 @@ exports.createTodo = async(req, res) => {
 }
 
 // todo 수정(update)
-exports.updateTodo = async(req, res) => {
-    console.log(req.params.todoId);
+// exports.updateTodo = async(req, res) => {
+//     console.log(req.params.todoId);
+//     try {
+//         // 변수가 아니라 배열로 담은 이유? 배열 구조 분해 
+//         // update() - 업데이트 된 행(row)의 수를 나타내는 값을 반환 -> 반환값은 배열 형태로 제공됨
+//         // 배열 구조 분해 할당을 통해 배열의 첫번째 요소를 변수에 할당 가능
+//         // [idUpdated] = [0] or [1]
+//         let [idUpdated] = await Todo.update(
+//             {
+//                 title : req.body.title, // 요청 본문에서 title 값을 가져와서 업데이트
+//                 done : req.body.done // 요청 본문에서 done 값을 가져와서 업데이트
+//             },
+//             {
+//                 where : {id : req.params.todoId }, // 경로 파라미터에서 'todo' 아이디를 사용해서 특정 todo 항목을 찾으셈 
+//                 // Op.eq -> Sequelize 의 연산자, 'equels'(같음) 이라는 뜻
+//             }
+//         );
+//         console.log(idUpdated);
+//         if(idUpdated === 0){ // 수정 실패
+//             return res.send(false) // 업데이트 된 항목이 없으면 false 반환
+//         }
+//         // 수정 성공
+//         res.send(true) // 업데이트가 성공하면 true 반환
+//     } catch (error) {
+//         res.send(error)
+//     }
+// }
+// // todo 삭제(delete)
+// exports.deleteTodo = async(req, res) => {
+//     try {
+//         let isDeleted = await Todo.destoy({ where : {id : req.params.todoId}})
+//         // 삭제 실패
+//         if(!isDeleted) {
+//             return res.send(false)
+//         }
+//         // 삭제 
+//         res.send(true)
+//     } catch (error) {
+//         res.send(error)
+//     }
+// }
+
+exports.updateTodo = async (req, res) => {
+    console.log('req.body >>>>> ', req.body);
     try {
-        // 변수가 아니라 배열로 담은 이유? 배열 구조 분해 
-        // update() - 업데이트 된 행(row)의 수를 나타내는 값을 반환 -> 반환값은 배열 형태로 제공됨
-        // 배열 구조 분해 할당을 통해 배열의 첫번째 요소를 변수에 할당 가능
-        // [idUpdated] = [0] or [1]
-        let [idUpdated] = await Todo.update(
-            {
-                title : req.body.title, // 요청 본문에서 title 값을 가져와서 업데이트
-                done : req.body.done // 요청 본문에서 done 값을 가져와서 업데이트
-            },
-            {
-                where : {id : req.params.todoId }, // 경로 파라미터에서 'todo' 아이디를 사용해서 특정 todo 항목을 찾으셈 
-                // Op.eq -> Sequelize 의 연산자, 'equels'(같음) 이라는 뜻
-            }
-        );
-        console.log(idUpdated);
-        if(idUpdated === 0){ // 수정 실패
-            return res.send(false) // 업데이트 된 항목이 없으면 false 반환
-        }
-        // 수정 성공
-        res.send(true) // 업데이트가 성공하면 true 반환
+      // 배열 구조 분해
+      // update() - 업데이트 된 행(row)의 수를 나타내는 값을 반환.
+      // 그 반환 값은 배열 형태로 제공.
+      // 배열 구조 분해 할당을 통해 배열의 첫 번째 요소를 변수에 할당할 수 있음.
+      // [idUpdated] = [ 0 ] or [ 1 ]
+      let [idUpdated] = await Todo.update(
+        {
+          title: req.body.title, // 요청 본문에서 'title' 값을 가져와서 업데이트
+          done: req.body.done, // 요청 본문에서 'done'값을 가져와서 업데이트
+        },
+        {
+          where: {
+            id: { [Op.eq]: req.params.todoId }, // 경로 파라미터에서 'todoId'를 사용하여 특정 Todo 항목을 찾음.
+            // Op.eq는 Sequelize의 연산자로, "equals"(같음) 라는 뜻.
+          },
+        },
+      );
+      console.log('idUpdated >>>>> ', idUpdated);
+  
+      // 수정 실패
+      if (idUpdated === 0) {
+        return res.send(false);
+        // 업데이트 된 항목이 없으면 'false'를 반환
+      }
+      // 수정 성공
+      res.send(true);
+      // 업데이트가 성공적으로 이루어지면 'true'를 반환.
     } catch (error) {
-        res.send(error)
+      res.send(error);
     }
-}
-// todo 삭제(delete)
-exports.deleteTodo = async(req, res) => {
+  };
+  
+  exports.deleteTodo = async (req, res) => {
     try {
-        let isDeleted = await Todo.destoy({ where : {id : req.params.todoId}})
-        // 삭제 실패
-        if(!isDeleted) {
-            return res.send(false)
-        }
-        // 삭제 
-        res.send(true)
+      let isDeleted = await Todo.destroy({
+        where: {
+          id: { [Op.eq]: req.params.todoId },
+        },
+      });
+      // 삭제 실패
+      if (!isDeleted) {
+        return res.send(false);
+      }
+      // 삭제 성공
+      res.send(true);
     } catch (error) {
-        res.send(error)
+      res.send(error);
     }
-}
+  };

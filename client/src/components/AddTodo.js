7aa52f686944d241ccc8103398036e448974fab1
DAIN302 <dbgnfk12@gmail.com>
2024-08-16 15:34:34 +0900

    study : todo

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import axios from 'axios';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
--- client/src/components/MyTodo.js
+++ client/src/components/MyTodo.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { faCircle } from "@fortawesome/free-regular-svg-icons";
